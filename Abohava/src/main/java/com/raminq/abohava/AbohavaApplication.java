9a47a459dfb4a01654e4d9daf10b4ee98f3b97a3
raminqdev <ramin.qamari93@gmail.com>
2021-05-13 04:20:48 +0430

    ScheduledTasks.

--- Abohava/src/main/java/com/raminq/abohava/AbohavaApplication.java
+++ Abohava/src/main/java/com/raminq/abohava/AbohavaApplication.java
@@ -2,8 +2,10 @@ package com.raminq.abohava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AbohavaApplication {
 
 	public static void main(String[] args) {
--- Abohava/src/main/java/com/raminq/abohava/scheduledTasks/ScheduledTasks.java
+++ Abohava/src/main/java/com/raminq/abohava/scheduledTasks/ScheduledTasks.java
@@ -1,9 +1,15 @@
 package com.raminq.abohava.scheduledTasks;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import lombok.var;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,12 +26,33 @@ https://spring.io/guides/gs/scheduling-tasks/
 public class ScheduledTasks {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${openWeatherMap.Api}")
+    private String api;
+    @Value("${openWeatherMap.AppId}")
+    private String appId;
+
+//
+//    public ScheduledTasks(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
+
+    @Bean
+    public RestTemplate setTemplate() {
+        return new RestTemplate();
+    }
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+        var response = restTemplate
+                .getForObject(api + "?q=tehran,IR&units=metric&appid=" + appId + "&lang=fa", String.class);
+        log.info("The time is now {} " + api, dateFormat.format(new Date()));
+
+
+
+        log.info(response);
     }
 }
 
